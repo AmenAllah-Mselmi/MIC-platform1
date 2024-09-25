a4dc405b61bba32978e536d82bdc2346745f6ba3
AmenAllah-Mselmi <amenallahmselmi5@gmail.com>
2024-09-26 01:32:21 +0200

    loading and not found

--- app/(MICPlatform)/Member/layout.tsx
+++ app/(MICPlatform)/Member/layout.tsx
@@ -3,14 +3,16 @@ import Navbar from '../_MICcomponents/navbar/navbar';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
+    <>
+    <Navbar />
     <div
       style={{ backgroundImage: 'url("/images/Member/MemberBackground.png")' }}
       className='w-screen min-h-screen bg-cover bg-center z--10 flex justify-center items-center py-20'
     >
-      <Navbar />
       {children}
 
     </div>
+    </>
   );
 };
 
