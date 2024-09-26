5967e6321117459a6d2f954b8b4c5ae37e4f7bcd
MariemTlatli <tlatlimariem3@gmail.com>
2024-09-26 22:31:08 +0100

    Ajout du composant Member_card et l'integrer dans page showMembers

--- app/(MICPlatform)/Instructor/showMembers/page.tsx
+++ app/(MICPlatform)/Instructor/showMembers/page.tsx
@@ -1,10 +1,83 @@
 import React from 'react'
-
+import { Container, Grid, Typography } from '@mui/material'
+import Member_card from '../../_MICcomponents/Member_card/Member_card'
 const page = () => {
+  const members = [
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      className: '3ème année Informatique',
+      image: 'https://randomuser.me/api/portraits/men/1.jpg'
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Smith',
+      className: '2ème année Génie Logiciel',
+      image: 'https://randomuser.me/api/portraits/women/2.jpg'
+    },
+    {
+      firstName: 'Alice',
+      lastName: 'Johnson',
+      className: '1ère année Réseaux',
+      image: 'https://randomuser.me/api/portraits/women/3.jpg'
+    },
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      className: '3ème année Informatique',
+      image: 'https://randomuser.me/api/portraits/men/1.jpg'
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Smith',
+      className: '2ème année Génie Logiciel',
+      image: 'https://randomuser.me/api/portraits/women/2.jpg'
+    },
+    {
+      firstName: 'Alice',
+      lastName: 'Johnson',
+      className: '1ère année Réseaux',
+      image: 'https://randomuser.me/api/portraits/women/3.jpg'
+    },
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      className: '3ème année Informatique',
+      image: 'https://randomuser.me/api/portraits/men/1.jpg'
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Smith',
+      className: '2ème année Génie Logiciel',
+      image: 'https://randomuser.me/api/portraits/women/2.jpg'
+    },
+    {
+      firstName: 'Alice',
+      lastName: 'Johnson',
+      className: '1ère année Réseaux',
+      image: 'https://randomuser.me/api/portraits/women/3.jpg'
+    }
+    // Ajouter d'autres membres ici
+  ]
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Typography
+        variant='h4'
+        component='h1'
+        align='center'
+        gutterBottom
+        sx={{ marginTop: '20px', fontWeight: 'bold', color: '#fff' }}
+      >
+        Membres du Département
+      </Typography>
+      <Grid container spacing={2}>
+        {members.map((member, index) => (
+          <Grid item xs={12} sm={4} md={2} key={index}>
+            <Member_card member={member} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   )
 }
 
